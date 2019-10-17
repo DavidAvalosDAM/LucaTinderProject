a38a6d1b7e73a352317a79c4c643a1d874ffc505
David <dam.avalos@gmail.es>
2019-10-17 18:34:11 +0200

    usuarioService e interface a medias

---
+++ src/main/java/com/lucatinder/service/UsuarioServiceImpl.java
@@ -0,0 +1,16 @@
+package com.lucatinder.service;
+
+import com.lucatinder.model.Persona;
+
+public class UsuarioServiceImpl implements UsuarioService {
+
+	public UsuarioServiceImpl() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Persona guardarPersona(Persona p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
