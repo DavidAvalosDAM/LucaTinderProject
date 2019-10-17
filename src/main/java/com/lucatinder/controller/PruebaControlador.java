06dfd3a9afff1b02d6d6687865e01e4b7a2cd166
Jorge <pruebasluca@gmail.com>
2019-10-17 18:33:46 +0200

    Crear Interface IUsuarioDao

--- src/main/java/com/lucatinder/controller/PruebaControlador.java
+++ src/main/java/com/lucatinder/controller/PruebaControlador.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.lucatinder.dao.IPersonaRepo;
+import com.lucatinder.dao.IUsuarioDao;
 import com.lucatinder.model.Persona;
 
 @Controller
--- src/main/java/com/lucatinder/dao/IPersonaRepo.java
+++
@@ -1,9 +0,0 @@
-package com.lucatinder.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.lucatinder.model.Persona;
-
-public interface IPersonaRepo extends JpaRepository<Persona, Integer> {
-	
-}
