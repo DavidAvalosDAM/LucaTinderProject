519d02dd1041518c0d18cd4de29c99ab59a263ee
David <dam.avalos@gmail.es>
2019-10-17 19:11:40 +0200

    commitPrueba

--- src/main/java/com/lucatinder/service/UsuarioServiceImpl.java
+++ src/main/java/com/lucatinder/service/UsuarioServiceImpl.java
@@ -7,9 +7,9 @@ public class UsuarioServiceImpl implements UsuarioService {
 	public UsuarioServiceImpl() {
 		// TODO Auto-generated constructor stub
 	}
-	
 	public Persona guardarPersona(Persona p) {
 		// TODO Auto-generated method stub
+		System.out.println("prueba");
 		return null;
 	}
 
