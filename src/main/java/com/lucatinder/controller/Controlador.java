c1ce5a4a124a0adfd08d82049f853e29d0d249f9
IVAN <ivan.job@outlook.es>
2019-10-18 13:49:23 +0200

    1.1.9 finalizado

--- src/main/java/com/lucatinder/controller/Controlador.java
+++ src/main/java/com/lucatinder/controller/Controlador.java
@@ -4,21 +4,44 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import com.lucatinder.dao.IPersonaRepo;
-import com.lucatinder.model.Persona;
+import com.lucatinder.dao.IUsuarioDao;
+import com.lucatinder.model.Usuario;
 
 @Controller
 public class Controlador {
 	
 	@Autowired
-	private IUsuario dao;
+	private IUsuarioDao dao;
 	
 	@GetMapping("/")
-	public String  urlLogin(Usuario u, Model model) {
-		dao.save(u);
-		model.addAttribute("usuario",u);
-		return "index";
+	public String  urlLogin(Model model) {
+				
+		model.addAttribute("usuario",new Usuario());
+		model.addAttribute("status","");
+		return "login";
+	}
+	
+	@PostMapping("/")
+	public String  urlLoginEnviado(Usuario u,Model model) {
+		
+			
+		if (u.getPassword().equals(dao.buscaPorNombre(u.getNombre()).getPassword())){
+			
+			model.addAttribute("usuario",u);
+			
+			return "index";
+		}
+		else {
+			
+			model.addAttribute("usuario",new Usuario());
+			model.addAttribute("status","El usuario o la contraseña son incorrectos");
+			
+			return "login";
+		}
+
+		
 	}
 	
 	/*Es necesario para que el formulario funcione.
--- src/main/java/com/lucatinder/dao/IUsuarioDao.java
+++ src/main/java/com/lucatinder/dao/IUsuarioDao.java
@@ -1,9 +1,13 @@
 package com.lucatinder.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.lucatinder.model.Usuario;
 
 public interface IUsuarioDao extends JpaRepository<Usuario, Integer> {
-	
+	@Query(
+			  value = "SELECT * FROM usuarios WHERE nombre = ?1", 
+			  nativeQuery = true)
+			Usuario buscaPorNombre(String nombre);
 }
