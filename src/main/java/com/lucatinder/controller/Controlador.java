a78676238f5ef8f989a410236de053a390b5bffc
Jorge <pruebasluca@gmail.com>
2019-10-20 19:16:31 +0200

    Revert "Merge branch 'ivan' into jorge"
    This reverts commit 356ca8215303088d9d335c1646ba6916d67aca60, reversing
    changes made to b9fb690343d918d1a79dd2588d5056f1c517cd77.

--- src/main/java/com/lucatinder/controller/Controlador.java
+++ src/main/java/com/lucatinder/controller/Controlador.java
@@ -11,78 +11,47 @@ import com.lucatinder.model.Usuario;
 import com.lucatinder.service.UsuarioService;
 
 @Controller
-/**
- * En esta clase se realizaran todas las operaciones de control.
- * @author Iván
- * @version 1.0
- * @date 18/10/2019
- */
 public class Controlador {
-
+	
 	@Autowired
 	private IUsuarioDao dao;
-
+	
 	@Autowired
 	private UsuarioService usi;
-
-	/**
-	 *
-	 * Método para entrar en login.
-	 * 
-	 * @version 1.0
-	 * @date 18/10/2019
-	 * @autor Ivan
-	 *
-	 */
+	
 	@GetMapping("/")
-	public String urlLogin(Model model) {
-		model.addAttribute("usuario", new Usuario());
-		model.addAttribute("status", "");
+	public String  urlLogin(Model model) {		
+		model.addAttribute("usuario",new Usuario());
+		model.addAttribute("status","");
 		return "login";
 	}
-
-	/**
-	 * Metodo para comprobar usuario y contraseña.
-	 * 
-	 * @version 1.0
-	 * @date 18/10/2019
-	 * @autor Ivan
-	 */
-	@PostMapping("/")
-	public String urlLoginEnviado(Usuario u, Model model) {
-		if (u.getPassword().equals(dao.buscaPorNombre(u.getNombre()).getPassword())) {
-			model.addAttribute("usuario", u);
+	
+	/*@PostMapping("/")
+	public String  urlLoginEnviado(Usuario u,Model model) {	
+		if (u.getPassword().equals(dao.buscaPorNombre(u.getNombre()).getPassword())){
+			model.addAttribute("usuario",u);
 			return "index";
-		} else {
-			model.addAttribute("usuario", new Usuario());
-			model.addAttribute("status", "El usuario o la contraseña son incorrectos");
-			return "login";
 		}
-	}
-
-	/**
-	 * Los siguientes 2 métodos sirven para en caso de no estar registrado te puedas
-	 * registrar (guardar usuario).
-	 * 
-	 * @version 1.0
-	 * @date 18/10/2019
-	 * @autor Iván
-	 */
+		else {
+			model.addAttribute("usuario",new Usuario());
+			model.addAttribute("status","El usuario o la contraseña son incorrectos");
+			return "login";
+		}	
+	}*/
+	
 	@GetMapping("/alta")
 	public String urlAlta(Model model) {
 		model.addAttribute("usuario", new Usuario());
 		return "formularioAlta";
 	}
-
+	
 	@PostMapping("/alta")
 	public String urlAltaRecibido(Model model, Usuario u) {
 		usi.guardarUsuario(u);
 		return "formularioAlta";
 	}
-
 	/**
 	 * Método creado para mostrar en html los datos del usuario
-	 * 
 	 * @version 1.0
 	 * @date 20/10/2019
 	 * @autor Yolanda
