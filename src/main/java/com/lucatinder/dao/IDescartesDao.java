3f6d74b95321060424f8728fb850d0ce7093ebcc
Yolanda <yolyguti79@hotmail.com>
2019-10-21 14:41:16 +0200

    Create IDescartesDao.java

---
+++ src/main/java/com/lucatinder/dao/IDescartesDao.java
@@ -0,0 +1,11 @@
+package com.lucatinder.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lucatinder.model.Descartes;
+
+public interface IDescartesDao extends JpaRepository<Descartes, Integer>  {
+	
+	
+
+}
